24e10d7897b8f51e1b2fca4217b74b12ce38f833
yishizu <48244588+yishizu@users.noreply.github.com>
2022-02-27 22:15:58 +0900

    Update snippet.js

--- Day01/snippet.js
+++ Day01/snippet.js
@@ -31,3 +31,73 @@ let data={trial1:[[1,2],[3,4]],trial2:[[5,6],[7,8]]};
 console.log(data);
 
 3+2 //=>4: 加算
+
+class Point{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    distance(){
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+}
+
+let p = new Point(1,1);
+p.distance();
+console.log(p.distance());
+
+class DefaultMap extends Map{
+    constructor(defaultValue){
+        super();
+        this.defaultValue = defaultValue;
+    }
+    get(key){
+        if(this.has(key)){
+            return super.get(key);
+        }else{
+            return this.defaultValue;
+        }
+    }
+}
+
+class Histogram{
+    constructor(){
+        this.letterCounts = new DefaultMap(0);
+        this.totalLetters = 0;
+    }
+    //
+    add(text){
+        text = text.replace(/\s/g,"").toUpperCase();
+        //
+        for(let character of text){
+            let count = this.letterCounts.get(character);
+            this.letterCounts.set(character, count+1);
+            this.totalLetters++;
+        }
+    }
+    //
+    toString(){
+        //
+        let entries = [...this.letterCounts];
+        //
+        entries.sort((a,b)=>{
+            if(a[1]===b[1]){
+                return a[0] <b[0] ? -1:1;
+            }else{
+                return b[1] -a[1];
+            }
+        });
+        //
+        for(let entry of entries){
+            entry[1] = entry[1]/this.totalLetters*100;
+        }
+        //
+        entries = entries.filter(entry => entry[1] >=1);
+
+        let lines = entries.map(
+            ([l,n]) => `${l}: ${"#".repeat(Math.round(n))} ${n.toFixed(2)}%`);
+
+        return lines.join("/n");
+        
+    }
+}
